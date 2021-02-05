0462df50f9bd3a2bf1513ba9603f388918c6498e
unknown <g00048625@gmit.ie>
2021-02-05 22:01:34 +0000

    new file length

---
+++ labsweek03/len.py
@@ -0,0 +1,12 @@
+# program reads in a string and then outputs its length/ creating and outputting a string 
+# Author Catherine Leddy
+
+inputString = input('enter a string:')
+lenghtOfString = len(inputString)
+print('The lenght of {} is {}'.format(inputString, lenghtOfString))
+
+message = 'Kate said\t"hi"\nSean said\t"bye"'
+print (message)
+
+
+
