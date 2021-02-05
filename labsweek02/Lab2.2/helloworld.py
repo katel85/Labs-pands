0953afe7e38510b3f78bc0bd28da318206c7551b
unknown <g00048625@gmit.ie>
2021-02-05 15:02:56 +0000

    week 02 labs

---
+++ labsweek02/Lab2.2/helloworld.py
@@ -0,0 +1,16 @@
+# lab 2.2 first programs practice
+# Catherine Leddy
+
+print ('hello world')
+
+#File ".\Lab2.2.py", line 4   print "hello world"          ^
+#SyntaxError: Missing parentheses in call to 'print'. Did you mean print("hello world")?
+# error forgot to use brackets for the output
+
+print ('hello')
+print ( 'Hello Kate')
+
+# Traceback (most recent call last):   File ".\Lab2.2.py", line 11, in <module>     print ( Hello + Kate) 
+# NameError: name 'Hello' is not defined
+# error did not use the comma when typing in the hello Kate
+
---
+++ labsweek02/Lab2.2/multiply.py
@@ -0,0 +1,11 @@
+# multiply using python 
+# Author Catherine Leddy
+
+answer=111*555
+
+# no output because we have not asked vscode to give an output to the problem
+# fix by asking the program to print the answer as an input
+
+print= input( answer )
+
+
